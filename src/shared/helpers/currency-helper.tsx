e7b8ef1a503d59edb57d0a71e38ac9f98639d791
Lucas Pedro da Hora <lucaspedro517@gmail.com>
2024-11-09 13:29:01 -0300

    ✨Feat: Adding docker image

--- src/shared/helpers/currency-helper.tsx
+++ src/shared/helpers/currency-helper.tsx
@@ -1,6 +1,6 @@
 export class CurrencyHelper {
   public formatCurrency(value: number): string {
-    let USDollar = new Intl.NumberFormat("en-US", {
+    const USDollar = new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
     });
